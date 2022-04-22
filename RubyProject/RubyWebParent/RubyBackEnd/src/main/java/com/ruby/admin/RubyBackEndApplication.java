21ecfef6b86c8012f14a3366f1788cd9fc59fab1
AnhThong1503 <truonghuynhanhthong@gmail.com>
2022-04-23 03:30:34 +0700

    Setup cấu hình phần mềm

---
+++ RubyProject/RubyWebParent/RubyBackEnd/src/main/java/com/ruby/admin/RubyBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ruby.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubyBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubyBackEndApplication.class, args);
+	}
+
+}

99a34ef1d8db274d034bbc234897b76ef3390ee6
AnhThong1503 <truonghuynhanhthong@gmail.com>
2022-04-24 00:59:17 +0700

    Code test case để thêm phần phân quyền trong database

--- RubyProject/RubyWebParent/RubyBackEnd/src/main/java/com/ruby/admin/RubyBackEndApplication.java
+++ RubyProject/RubyWebParent/RubyBackEnd/src/main/java/com/ruby/admin/RubyBackEndApplication.java
@@ -2,8 +2,10 @@ package com.ruby.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.ruby.common.entity", "com.ruby.admin.user"})
 public class RubyBackEndApplication {
 
 	public static void main(String[] args) {

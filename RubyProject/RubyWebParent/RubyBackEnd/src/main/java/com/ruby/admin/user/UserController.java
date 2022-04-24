c1023beecd68141631475dcf9e0936b0c7a5f81d
AnhThong1503 <truonghuynhanhthong@gmail.com>
2022-04-24 13:53:50 +0700

    Hoàn thành phần thêm User trong Admin

--- RubyProject/RubyWebParent/RubyBackEnd/src/main/java/com/ruby/admin/user/UserController.java
+++ RubyProject/RubyWebParent/RubyBackEnd/src/main/java/com/ruby/admin/user/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ruby.common.entity.User;
 
@@ -20,5 +22,27 @@ public class UserController {
 		model.addAttribute("listUsers", listUsers);
 
 		return "users";
+	}
+
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		List<com.ruby.common.entity.Role> listRoles = service.listRoles();
+
+		User user = new User();
+		user.setEnabled(true);
+
+		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
+
+		return "user_form";
+	}
+
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user);
+		service.save(user);
+
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+		return "redirect:/users";
 	}
 }

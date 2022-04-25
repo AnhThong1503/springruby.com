402a2bf56dec0626a96fe765def9cf443179c039
AnhThong1503 <truonghuynhanhthong@gmail.com>
2022-04-25 23:32:39 +0700

    Delete User

--- RubyProject/RubyWebParent/RubyBackEnd/src/main/java/com/ruby/admin/user/UserService.java
+++ RubyProject/RubyWebParent/RubyBackEnd/src/main/java/com/ruby/admin/user/UserService.java
@@ -81,4 +81,13 @@ public class UserService {
 		}
 	}
 
+	public void delete(Integer id) throws UserNotFoundException {
+		Long countById = userRepo.countById(id);
+		if (countById == null || countById == 0) {
+			throw new UserNotFoundException("Couldn't not find any user with ID " + id);
+		}
+
+		userRepo.deleteById(id);
+	}
+
 }

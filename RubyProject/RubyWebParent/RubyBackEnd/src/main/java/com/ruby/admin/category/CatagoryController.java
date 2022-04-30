9ba68744f5936d954fea311ee034fa6be988be95
AnhThong1503 <truonghuynhanhthong@gmail.com>
2022-04-30 16:19:40 +0700

    Browse File, and upload picture to DB form category

--- RubyProject/RubyWebParent/RubyBackEnd/src/main/java/com/ruby/admin/category/CatagoryController.java
+++ RubyProject/RubyWebParent/RubyBackEnd/src/main/java/com/ruby/admin/category/CatagoryController.java
@@ -1,12 +1,19 @@
 package com.ruby.admin.category;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.ruby.admin.FileUploadUtil;
 import com.ruby.common.entity.Category;
 
 @Controller
@@ -33,6 +40,21 @@ public class CatagoryController {
 		model.addAttribute("pageTitle", "Create New Category");
 
 		return "categories/category_form";
+	}
+
+	@PostMapping("/categories/save")
+	public String saveCategory(Category category, @RequestParam("fileImage") MultipartFile multipartFile,
+			RedirectAttributes ra) throws IOException {
+
+		String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+		category.setImage(fileName);
+
+		Category savedCategory = service.save(category);
+		String uploadDir = "../category-images/" + savedCategory.getId();
+		FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+
+		ra.addFlashAttribute("message", "The caterogy has been saved sucessfully");
+		return "redirect:/categories";
 	}
 
 }

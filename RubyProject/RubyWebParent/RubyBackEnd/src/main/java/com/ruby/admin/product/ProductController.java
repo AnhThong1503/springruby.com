79e4f675604bf639a8c6022acd62a7ed886c6cd9
AnhThong1503 <truonghuynhanhthong@gmail.com>
2022-05-04 16:26:03 +0700

    Product_Form Update 1

--- RubyProject/RubyWebParent/RubyBackEnd/src/main/java/com/ruby/admin/product/ProductController.java
+++ RubyProject/RubyWebParent/RubyBackEnd/src/main/java/com/ruby/admin/product/ProductController.java
@@ -7,10 +7,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.ruby.admin.brand.BrandService;
+import com.ruby.common.entity.Brand;
 import com.ruby.common.entity.Product;
 
 @Controller
 public class ProductController {
+
+	@Autowired
+	private BrandService brandService;
 
 	@Autowired
 	private ProductService productService;
@@ -22,5 +27,20 @@ public class ProductController {
 		model.addAttribute("listProducts", listProducts);
 
 		return "products/products";
+	}
+
+	@GetMapping("/products/new")
+	public String newProduct(Model model) {
+		List<Brand> listBrands = brandService.listAll();
+
+		Product product = new Product();
+		product.setEnabled(true);
+		product.setInStock(true);
+
+		model.addAttribute("product", product);
+		model.addAttribute("listBrands", listBrands);
+		model.addAttribute("pageTitle", "Create New Product");
+
+		return "products/product_form";
 	}
 }

9f1524bf54fdf12b686fee85d314ee6a8667f17a
AnhThong1503 <truonghuynhanhthong@gmail.com>
2022-05-06 17:26:26 +0700

    Update sorting and modal for product

--- RubyProject/RubyWebParent/RubyBackEnd/src/main/java/com/ruby/admin/product/ProductController.java
+++ RubyProject/RubyWebParent/RubyBackEnd/src/main/java/com/ruby/admin/product/ProductController.java
@@ -11,6 +11,8 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -40,9 +42,38 @@ public class ProductController {
 
 	@GetMapping("/products")
 	public String listAll(Model model) {
-		List<Product> listProducts = productService.listAll();
+		return listByPage(1, model, "name", "asc", null);
+	}
 
+	@GetMapping("/products/page/{pageNum}")
+	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model,
+			@Param("sortField") String sortField, @Param("sortDir") String sortDir, @Param("keyword") String keyword) {
+		System.out.println("Sort Field: " + sortField);
+		System.out.println("Sort Order: " + sortDir);
+
+		Page<Product> page = productService.listByPage(pageNum, sortField, sortDir, keyword);
+
+		List<Product> listProducts = page.getContent();
+
+		long startCount = (pageNum - 1) * productService.PRODUCTS_PER_PAGE + 1;
+		long endCount = startCount + productService.PRODUCTS_PER_PAGE - 1;
+
+		if (endCount > page.getTotalElements()) {
+			endCount = page.getTotalElements();
+		}
+
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("totalPage", page.getTotalPages());
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItems", page.getTotalElements());
 		model.addAttribute("listProducts", listProducts);
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", reverseSortDir);
+		model.addAttribute("keyword", keyword);
 
 		return "products/products";
 	}
@@ -58,6 +89,7 @@ public class ProductController {
 		model.addAttribute("product", product);
 		model.addAttribute("listBrands", listBrands);
 		model.addAttribute("pageTitle", "Create New Product");
+		model.addAttribute("numberOfExistingExtraImages", 0);
 
 		return "products/product_form";
 	}
@@ -246,4 +278,18 @@ public class ProductController {
 		}
 	}
 
+	@GetMapping("/products/detail/{id}")
+	public String viewProductDetails(@PathVariable("id") Integer id, Model model, RedirectAttributes ra) {
+		try {
+			Product product = productService.get(id);
+
+			model.addAttribute("product", product);
+
+			return "products/product_detail_modal";
+		} catch (ProductNotFoundException e) {
+			ra.addFlashAttribute("message", e.getMessage());
+
+			return "redirect:/products";
+		}
+	}
 }
